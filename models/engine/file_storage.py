b9e5798cc614a18d92dc27186ce79a04a54cb570
Falendrac <leger_franck@hotmail.fr>
2022-04-11 10:37:18 +0200

    Update fileStorage, adding delete func and change all.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from unicodedata import name
 
 
 class FileStorage:
@@ -8,8 +9,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            tmpObj = {}
+            for key, val in self.__objects.items():
+                if cls is type(self.__objects[key]):
+                    tmpObj[key] = val
+            return tmpObj
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +55,12 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside"""
+        if obj != None:
+            for key in self.__objects.keys():
+                if obj.id == self.__objects[key].id:
+                    del(self.__objects[key])
+                    self.save()
+                    return
