febccf92db2605eb12177b89fc19af37cd1165e5
Falendrac <leger_franck@hotmail.fr>
2022-04-13 15:15:18 +0200

    Update all test and add delete function.

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 from time import sleep
 import unittest
 from models.base_model import storage_Type
+from models.engine.db_storage import DBStorage
 from models.state import State
 import MySQLdb
 import os
@@ -28,6 +29,36 @@ class test_db_storage(unittest.TestCase):
             db=db_test,
             charset="utf8")
         cur = db.cursor()
+
+    def test_doc(self):
+        """
+        Check all the doc of the Amenity Class
+        """
+        # module documentation
+        module = len(DBStorage.__doc__)
+        self.assertGreater(module, 0)
+
+        # class documentation
+        module_class = len(DBStorage.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.new.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.save.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.delete.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.reload.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.all.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.__init__.__doc__)
+        self.assertGreater(module_class, 0)
 
     @unittest.skipIf(storage_Type != "db", "don't test with filestorage")
     def test_count_row(self):
