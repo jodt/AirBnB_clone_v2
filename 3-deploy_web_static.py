8b83a1b3d417eac6b48cf5b7326f0a52f8af433f
jodt <joel.dumortier@gmail.com>
2022-04-27 22:33:53 +0000

    bug fix import

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,13 +3,52 @@
 This is the deploy_web_static module
 """
 from fabric.api import *
-
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+import datetime
+from os.path import exists
 
 env.hosts = ["35.227.90.48", "34.139.233.64"]
 
+
+def do_pack():
+    """
+    This function create an archive
+    """
+    now = datetime.datetime.now()
+    now_str = "{}{}{}{}{}{}".format(
+        now.year, now.month, now.day, now.hour, now.minute, now.second)
+    name_file = "web_static_"+now_str+".tgz"
+    local("mkdir -p versions")
+    local("tar -cvzf versions/{} web_static".format(name_file))
+    return "versions/{}".format(name_file)
+
+
 path_archive = do_pack()
+
+
+def do_deploy(archive_path):
+    """
+    This function distributes an archive to the web servers,
+    """
+    if not exists(archive_path):
+        return False
+    try:
+        archive_name = archive_path.split("/")[-1][:-4]
+        folder = "/data/web_static/releases/"
+        put(archive_path, "/tmp")
+        run("mkdir -p {}{}/".format(folder, archive_name))
+        run("tar -xzf /tmp/{}.tgz -C {}{}".format(archive_name, folder,
+                                                  archive_name))
+        run("rm /tmp/{}.tgz".format(archive_name))
+        run("mv {}{}/web_static/* {}{}/".format(folder,
+            archive_name, folder, archive_name))
+        run("rm -rf {}{}/web_static".format(folder, archive_name))
+        run("rm /data/web_static/current")
+        run("ln -s -f {}{}/ /data/web_static/current".format(folder,
+                                                             archive_name))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
 
 
 def deploy():
