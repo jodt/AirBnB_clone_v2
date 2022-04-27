4c888b10cc42186cb5868b94cd887856749fbdc8
jodt <joel.dumortier@gmail.com>
2022-04-27 22:25:01 +0000

    add flag chown instruction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/ 
 echo "hello world" > /data/web_static/releases/test/index.html
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu: /data/
+chown -Rh ubuntu: /data/
 sed -i '56 i location /hbnb_static {\n\t alias /data/web_static/current;/\n}' /etc/nginx/sites-enabled/default
 service nginx restart
